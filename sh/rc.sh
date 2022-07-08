a84c82d88bee844818dab50530eb2983c3fc1721
leastfavorite <ariaschnei@gmail.com>
2022-07-08 00:21:32 -0500

    shell-independent setup

--- sh/rc.sh
+++ sh/rc.sh
@@ -40,6 +40,7 @@ case $DF_OS in
         install_pkg bat
         install_pkg node
         install_pkg yarn
+	install_pkg wget
 
         if [ -s "$DF_PKGS_TO_INSTALL" ]; then
 	    brew install `cat $DF_PKGS_TO_INSTALL`
@@ -56,3 +57,66 @@ case $DF_SHELL in
     *) DF_DIR="$0" ;;
 esac
 export DF_DIR=$(dirname -- $(dirname -- $(realpath -- $DF_DIR)))
+
+# ssh agent
+ssh_check() {
+    ! ssh-add -l >/dev/null && [ -d $HOME/.ssh ] && \
+        find "$HOME/.ssh" -name '*.pub' >/dev/null && \
+        info "Running ssh-agent..." && ssh-add;
+}
+ssh_check
+
+git config --global alias.checkeven "!sh $DF_DIR/sh/git_checkeven.sh"
+
+# check for updates
+(
+    cd $DF_DIR
+    git fetch
+    even=`git checkeven`
+    case $even in
+        *"behind"*) warn "Current dotfiles are behind origin. Consider updating." ;;
+        *"ahead"*) warn "Current dotfiles are ahead of origin. Consider pushing." ;;
+        *"diverge"*) warn "Current dotfiles have diverged from origin. Consider merging." ;;
+    esac
+)
+
+# yarn setup
+export DF_NPM_DIR="$DF_DIR/local/.npm_packages"
+mkdir -p "$DF_NPM_DIR"
+npm config set prefix "$DF_NPM_DIR"
+export PATH="$PATH:$DF_NPM_DIR/bin"
+export MANPATH="$MANPATH:$DF_NPM_DIR/share/man"
+
+# shell stuff
+export HISTCONTROL=ignoreboth:erasedups
+export SAVEHIST=9999999
+export HISTSIZE=9999999
+export EDITOR="nvim"
+export VISUAL="nvim"
+export LS_COLORS="$LS_COLORS:ow=1;94"
+
+# aliases
+alias ls='ls --color=auto'
+alias la='ls -a'
+alias ll='ls -la'
+alias l='ls'
+alias l.="ls -A | egrel '^\.'"
+
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+alias wget='wget -c'
+
+alias psa='ps auxf'
+alias psgrep='ps aux | grep -v grep | grep -i -e VSZ -e'
+
+alias rg='rg --sort path'
+alias todo='$EDITOR $HOME/todo.txt'
+
+# ssh wrapper with agent forwarding. automatically
+# tries to enter zsh
+function s() {
+    ssh_check
+    ssh -At $@ -- 'zsh -l'
+}
