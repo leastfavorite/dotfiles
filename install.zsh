5529780c16000c430dac420bb4f8f8e1dac3413e
ariaschnei <ariaschnei@gmail.com>
2023-06-06 23:20:49 -0400

    debian compatibility (loosely--probably doesnt work on a raw instance. needs more testing)

--- install.zsh
+++ install.zsh
@@ -1,7 +1,11 @@
 #!/bin/zsh
+set -e
 
 DF_DIR=${${(%):-%x}:A:h}
 path+="$DF_DIR/zsh/bin"
+path+="$DF_DIR/local/bin"
+
+git --git-dir="$DF_DIR/.git" submodule update --init --recursive
 
 # make symlink w/ backup
 make_link() {
@@ -11,18 +15,28 @@ make_link() {
     ln -s $1 $2
 }
 
+case $(os) in
+    mac) brew install kitty coreutils python fzf neovim ripgrep bat node yarn wget ;;
+    debian) 
+        sudo apt install -y curl
+        curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+        sudo apt install -y kitty fzf ripgrep bat nodejs fuse python3-pip
+        make_link /usr/bin/batcat "$DF_DIR/local/bin/bat"
+        sudo apt update && sudo apt upgrade
+        curl -fsSL https://github.com/neovim/neovim/releases/download/stable/nvim.appimage > "$DF_DIR/local/bin/nvim"
+        chmod u+x "$DF_DIR/local/bin/nvim"
+    ;;
+esac
+
 DF_PLUG_VIM="$DF_DIR/local/vim-plug.vim"
 if [[ ! -f $DF_PLUG_VIM ]]; then
     curl -fLo "$DF_PLUG_VIM" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-case $(os) in
-    mac) brew install kitty coreutils python fzf neovim ripgrep bat node yarn wget ;;
-esac
-
 export DF_NPM_DIR="$DF_DIR/local/.npm_packages"
 mkdir -p "$DF_NPM_DIR"
 npm config set prefix "$DF_NPM_DIR"
+npm install --global yarn
 
 make_link "$DF_PLUG_VIM" "$HOME/.local/share/nvim/site/autoload/plug.vim"
 make_link "$DF_DIR/zsh/rc" "$HOME/.zshrc"
