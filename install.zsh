cf8fb3f97c9314db7bc383b7ed7d9dd44a55bb02
leastfavorite <ariaschnei@gmail.com>
2023-10-19 04:41:42 -0400

    Install script modified for Fedora.

--- install.zsh
+++ install.zsh
@@ -16,20 +16,19 @@ make_link() {
 }
 
 case $(os) in
-    mac) brew install kitty coreutils python fzf neovim ripgrep bat wget ;;
-    debian) 
-        sudo apt install -y curl kitty fzf ripgrep bat fuse python3-pip
+    mac) brew install kitty coreutils python neovim ripgrep bat wget ;;
+    debian)
+        sudo apt install -y curl kitty ripgrep bat fuse python3-pip
         make_link /usr/bin/batcat "$DF_DIR/local/bin/bat"
         sudo apt update && sudo apt upgrade
         curl -fsSL https://github.com/neovim/neovim/releases/download/stable/nvim.appimage > "$DF_DIR/local/bin/nvim"
         chmod u+x "$DF_DIR/local/bin/nvim"
     ;;
+    fedora) sudo yum install -y kitty coreutils python python3-pip neovim ripgrep bat ;;
 esac
 
-make_link "$DF_PLUG_VIM" "$HOME/.local/share/nvim/site/autoload/plug.vim"
 make_link "$DF_DIR/zsh/rc" "$HOME/.zshrc"
-make_link "$DF_DIR/nvim/init.vim" "$HOME/.config/nvim/init.vim"
-make_link "$DF_DIR/nvim/coc-settings.json" "$HOME/.config/nvim/coc-settings.vim"
+make_link "$DF_DIR/nvim/init.lua" "$HOME/.config/nvim/init.lua"
 make_link "$DF_DIR/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
 make_link "$DF_DIR/misc/fly16.tmTheme" "$(bat --config-dir)/themes/fly16.tmTheme"
 make_link "$DF_DIR/kitty/vim-kitty-navigator/neighboring_window.py" "$HOME/.config/kitty/neighboring_window.py"
